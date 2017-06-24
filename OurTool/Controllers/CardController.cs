9d5fa174720829588e517f45ea4e3f01984d0edb
郭佳 <guo_jia_jia@hotmail.com>
2017-06-24 19:15:06 +0800

    创建实体

---
+++ OurTool/Controllers/CardController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OurTool.Controllers
+{
+    public class CardController : Controller
+    {
+        // GET: Card
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ OurToolModel/Card/CardUserRecode.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OurToolModel.Card
+{
+   public class CardUserRecode
+    {
+
+        /// <summary>
+        /// 用户ID
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// 用户ID
+        /// </summary>
+        public int UserId { get; set; }
+     
+        /// <summary>
+        /// 消费金额
+        /// </summary>
+        public decimal OptMoney { get; set; }
+
+        /// <summary>
+        /// 备注
+        /// </summary>
+        public string Mark { get; set; }
+        /// <summary>
+        /// 卡号
+        /// </summary>
+        public int CardNum { get; set; }
+        /// <summary>
+        /// 卡余额
+        /// </summary>
+        public int CardMoney { get; set; }
+
+        /// <summary>
+        /// 创建时间
+        /// </summary>
+        public DateTime CreateTime { get; set; }
+      
+    }
+}
