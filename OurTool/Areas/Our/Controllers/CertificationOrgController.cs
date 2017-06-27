1dec4e14ef18dcaa7b7e343299cbb868595c43bc
郭佳 <guo_jia_jia@hotmail.com>
2017-06-27 10:27:19 +0800

    优化

---
+++ OurTool/Areas/Our/Controllers/CertificationOrgController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OurTool.Areas.Our.Controllers
+{
+    public class CertificationOrgController : Controller
+    {
+        // GET: Our/CertificationOrg
+        /// <summary>
+        /// 认证培训机构
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Index()
+        {
+            return View();
+        }
+        /// <summary>
+        /// 名校大全
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult SchoolAll()
+        {
+            return View();
+        }
+    }
+}
---
+++ OurTool/Areas/Our/Controllers/ThreeStageController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OurTool.Areas.Our.Controllers
+{
+    public class ThreeStageController : Controller
+    {
+        // GET: Our/ThreeStage
+        /// <summary>
+        /// 幼儿园
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Index()
+        {
+            return View();
+        }
+        /// <summary>
+        /// 小学
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult SmallIndex()
+        {
+            return View();
+        }
+        /// <summary>
+        /// 初中
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult JuniorIndex()
+        {
+            return View();
+        }
+       /// <summary>
+       /// 高中
+       /// </summary>
+       /// <returns></returns>
+        public ActionResult SeniorIndex()
+        {
+            return View();
+        }
+    }
+}
