1a1ee60a6473427752350ff043a21448a092511a
郭佳 <guo_jia_jia@hotmail.com>
2017-06-28 20:37:49 +0800

    优化

--- OurTool/Controllers/HomeController.cs
+++ OurTool/Controllers/HomeController.cs
@@ -11,19 +11,8 @@ namespace OurTool.Controllers
     {
         public ActionResult Index()
         {
-            //string appId = System.Configuration.ConfigurationManager.AppSettings["WeChatAppId"];
-            //string appSecret = System.Configuration.ConfigurationManager.AppSettings["WeChatAppSecret"];
-            //bool debug = System.Configuration.ConfigurationManager.AppSettings["WeChatJsDebug"].ToLower() == "true";
-            //JSSDK sdk = new JSSDK(appId, appSecret, debug);
-            //SignPackage config = sdk.GetSignPackage(JsApiEnum.onMenuShareAppMessage | JsApiEnum.scanQRCode | JsApiEnum.onMenuShareQQ | JsApiEnum.onMenuShareTimeline);
-            //System.Web.Script.Serialization.JavaScriptSerializer serializer = new System.Web.Script.Serialization.JavaScriptSerializer();
-            //config.jsApiList = new string[] { "checkJsApi", "onMenuShareTimeline", "onMenuShareAppMessage", "onMenuShareQQ", "onMenuShareWeibo", "onMenuShareQZone", "hideMenuItems", "showMenuItems", "hideAllNonBaseMenuItem", "showAllNonBaseMenuItem", "translateVoice", "startRecord", "stopRecord", "onVoiceRecordEnd", "playVoice", "onVoicePlayEnd", "pauseVoice", "stopVoice", "uploadVoice", "downloadVoice", "chooseImage", "previewImage", "uploadImage", "downloadImage", "getNetworkType", "openLocation", "getLocation", "hideOptionMenu", "showOptionMenu", "closeWindow", "scanQRCode", "chooseWXPay", "openProductSpecificView", "addCard", "chooseCard", "openCard" };
-            ViewBag.config = GetWchatConfig(); //serializer.Serialize(config);
-            //var xx= Request.Url.ToString();//绝对路径
-
-            //ViewBag.Url = xx;
-            //ViewBag.Name = config.signature;
-
+           
+            ViewBag.config = GetWchatConfig(); 
             return View("Index");
         }
         /// <summary>
