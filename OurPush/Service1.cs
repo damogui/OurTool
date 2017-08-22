53059ed4b3bc14868f16368a84ad3d207ce85f15
郭佳 <guo_jia_jia@hotmail.com>
2017-08-22 15:19:55 +0800

    seo

--- OurPush/Service1.cs
+++ OurPush/Service1.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.ServiceProcess;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +14,10 @@ namespace OurPush
 {
     public partial class Service1 : ServiceBase
     {
+
+        private int flagPush = 0;//是否已经推送0今天未推送，1已经推送
+        //StudentManageBll _studentManageBll = new StudentManageBll();
+        int isP = Convert.ToInt32(System.Configuration.ConfigurationManager.AppSettings["isPush"]);
         public Service1()
         {
             InitializeComponent();
@@ -19,22 +25,46 @@ namespace OurPush
 
         protected override void OnStart(string[] args)
         {
+
+            System.Threading.Timer timer2 = new System.Threading.Timer(DoPush, null, 0, 1000 * 6);//6秒执行一次
         }
 
 
-       
+        /// <summary>
+        /// 进行推送
+        /// </summary>
+        private void DoPush(object state)
+        {
+            try
+            {
+                if (DateTime.Now.ToString("HH:mm") == "07:30" && flagPush == 0 && isP == 0)
+                {
+                    flagPush = 1;
+                    int execNum = 1; //_studentManageBll.GetPushStuWatch();//进行监督推送
+
+                }
+                if (DateTime.Now.ToString("HH:mm") == "23:30")
+                {
+                    flagPush = 0;//恢复原值
+                }
+
+            }
+            catch (Exception)
+            {
+
+
+            }
+        }
+
 
         protected override void OnStop()
         {
         }
 
-        /// <summary>
-        /// 进行推送
-        /// </summary>
-        private void DoPush()
-        {
+
+   
 
 
-        }
+
     }
 }
---
+++ OurToolCommon/PushCommon.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace OurToolCommon
+{
+   public class PushCommon
+    {
+
+        #region 微信消息模板
+        /// <summary>
+        /// 传递推送模板
+        /// </summary>
+        /// <param name="temp"></param>
+        /// <param name="appId"></param>
+        /// <param name="secret"></param>
+        /// <returns></returns>
+        public string SendTempletMessge(string temp, string appId, string secret)
+        {
+            string strReturn = string.Empty;
+            try
+            {
+                #region 获取access_token  
+                string apiurl = string.Format("https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid={0}&secret={1}", appId, secret);
+                WebRequest request = WebRequest.Create(@apiurl);
+                request.Method = "POST";
+                WebResponse response = request.GetResponse();
+                Stream stream = response.GetResponseStream();
+                Encoding encode = Encoding.UTF8;
+                StreamReader reader = new StreamReader(stream, encode);
+                string detail = reader.ReadToEnd();
+                var jd = JsonConvert.DeserializeObject<WXApi>(detail);
+                string token = (String)jd.access_token;
+                #endregion
+
+                #region 组装信息推送，并返回结果（其它模版消息于此类似）  
+                string url = "https://api.weixin.qq.com/cgi-bin/message/wxopen/template/send?access_token=" + token;
+                #endregion
+
+                //发送消息  
+                GetResponseData(temp, @url);
+                strReturn = "推送成功";
+            }
+            catch (Exception ex)
+            {
+                strReturn = ex.Message;
+            }
+            return strReturn;
+        }
+
+        /// <summary>    
+        /// 返回JSon数据    
+        /// </summary>    
+        /// <param name="JSONData">要处理的JSON数据</param>    
+        /// <param name="Url">要提交的URL</param>    
+        /// <returns>返回的JSON处理字符串</returns>    
+        public string GetResponseData(string JSONData, string Url)
+        {
+            byte[] bytes = Encoding.UTF8.GetBytes(JSONData);
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(Url);
+            request.Method = "POST";
+            request.ContentLength = bytes.Length;
+            request.ContentType = "json";
+            Stream reqstream = request.GetRequestStream();
+            reqstream.Write(bytes, 0, bytes.Length);
+
+            //声明一个HttpWebRequest请求    
+            request.Timeout = 90000;
+            //设置连接超时时间    
+            request.Headers.Set("Pragma", "no-cache");
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            Stream streamReceive = response.GetResponseStream();
+            Encoding encoding = Encoding.UTF8;
+
+            StreamReader streamReader = new StreamReader(streamReceive, encoding);
+            string strResult = streamReader.ReadToEnd();
+            streamReceive.Dispose();
+            streamReader.Dispose();
+            return strResult;
+        }
+
+        #endregion
+    }
+
+
+
+    public class WXApi
+    {
+        public string access_token { set; get; }
+    }
+}
