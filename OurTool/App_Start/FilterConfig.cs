234dece0b8c7507a5865e045f27c96a16ec65692
郭佳 <guo_jia_jia@hotmail.com>
2017-10-26 15:18:12 +0800

    解决跨域问题

--- OurTool/App_Start/FilterConfig.cs
+++ OurTool/App_Start/FilterConfig.cs
@@ -1,13 +1,34 @@
-﻿using System.Web;
+﻿
+using System;
+using System.Linq;
+using System.Web;
 using System.Web.Mvc;
-
 namespace OurTool
 {
     public class FilterConfig
     {
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
-            filters.Add(new HandleErrorAttribute());
+
+            filters.Add(new OrgAttribute());//请求页面的权限判断
+            //filters.Add(new OrgExceptionFilterAttribute());//异常
         }
+
+        /// <summary>
+        /// 全局处理
+        /// </summary>
+        public class OrgAttribute : ActionFilterAttribute
+        {
+            public override void OnActionExecuting(ActionExecutingContext filterContext)
+            {
+                filterContext.RequestContext.HttpContext.Response.AppendHeader("Access-Control-Allow-Origin", "*");
+                filterContext.RequestContext.HttpContext.Response.AppendHeader("Access-Control-AllowMethods","POST,GET,OPTIONS,DELETE");
+                filterContext.RequestContext.HttpContext.Response.AppendHeader("Access-Control-Allow-Headers", "x-requested-with,content-type");//解决跨域的问题
+             
+
+                base.OnActionExecuting(filterContext);
+            }
+        }
+
     }
 }
--- OurTool/Controllers/HomeController.cs
+++ OurTool/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Remoting.Messaging;
 using System.Text;
 using System.Web;
 using System.Web.Mvc;
@@ -602,6 +603,8 @@ namespace OurTool.Controllers
         /// <returns></returns>
         public JsonResult GetHotNews()
         {
+
+           
 
             JsonResult jsonResult=new JsonResult() {JsonRequestBehavior = JsonRequestBehavior.AllowGet};
 
